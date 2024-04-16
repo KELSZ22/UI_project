44f79debbbb7a5a792970ac4eeb7f77aa6f8882e
Nitsc Fullstack I <nitscfullstacki@Nitscs-iMac.local>
2024-04-16 09:35:36 +0800

    hi2

--- UI_Manpower/resources/views/cms/ClientData.blade.php
+++ UI_Manpower/resources/views/cms/ClientData.blade.php
@@ -133,7 +133,7 @@
                       </svg>
                                                                                                                     Home
                                                                                                                 </a>
-                                                                                                                <a href="/aboutus" class="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group">
+                                                                                                                <a href="/Aboutus" class="flex items-center px-4 py-2.5 text-sm font-medium transition-all duration-200 text-gray-900 hover:text-white rounded-lg hover:bg-indigo-600 group">
                                                                                                                 <img class="flex-shrink-0 w-5 h-5 mr-4 " itemprop="contentUrl" src="https://www.svgrepo.com/show/356999/about.svg" alt="About SVG File" title="About SVG File" width="150" height="150">
                                                                                                                     About Us
                                                                                                                 </a>
